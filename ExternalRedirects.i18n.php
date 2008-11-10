8c7c71522ef74f22dc67fb071786db5d7c98796a
Mathias Ertl <mati@fsinf.at>
2008-11-10 17:05:44 +0000

    1.5.1 * Special:ExternalRedirects now in Group VoWi-Extensions

--- ExternalRedirects.i18n.php
+++ ExternalRedirects.i18n.php
@@ -2,12 +2,16 @@
 $messages = array( 'en' => array(
 		'externalredirects' => 'Externalredirects',
 		'mExternalRedirectsDescription' => 'External Redirects',
+		'specialpages-specialpagegroup-externalredirects' => "vowi",
+		'specialpages-group-vowi' => "VoWi-Extensions"
 	),
 	'de' => array(
 #		'externalredirects' => 'Externe Weiterleitungen',
 		'externalredirects' => 'Externalredirects',
 #		'externe_weiterleitungen' => 'Externe Weiterleitungen',
 		'mExternalRedirectsDescription' => 'Externe Weiterleitungen',
+		'specialpages-specialpagegroup-externalredirects' => "vowi",
+		'specialpages-group-vowi' => "VoWi-Extensions"
 	),
 );
 ?>
